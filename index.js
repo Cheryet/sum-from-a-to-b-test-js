f1450053c488c6d6f94f096cf2d3a09de1d56622
Cheryet <C.heryet@outlook.com>
2022-10-21 19:46:47 -0600

    Completed sum test

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let total = 0;
+  total += fromN;
+
+  if (fromN >= toN) {
+    return total;
+  }
+
+  return total + sum(fromN + 1, toN);
+
 }
 
 module.exports = sum;
